5786b1702513d87555f12ff92ff367c3f3d17cf6
Prtfw <shang.madeleine@gmail.com>
2018-04-25 16:01:22 -0700

    phase2

--- dataset_wrapper.py
+++ dataset_wrapper.py
@@ -42,30 +42,33 @@ def match_dicom_path(_path_to_contour):
         return None, None
     return folder, filename
 
-def makeData(dic, cont):
+def make_data(dic, cont):
     '''returns data as DataSet object'''
     new_data_set = batching.DataSet(dic, cont)
     return new_data_set
 
 
-def batch_wrapper(_LINKFILE_PATH):
+def batch_wrapper(_linkfile_path, contour_type='o'):
     '''main function to call, returns DataSet Obeject'''
     all_path2contours = []
-    contour_files, _, pairs = get_file_pairs(_LINKFILE_PATH)
-    contour_paths = ['./final_data/contourfiles/'+ x +'/i-contours' for x in contour_files]
+    contour_files, _, pairs = get_file_pairs(_linkfile_path)
+    contour_paths = ['./final_data/contourfiles/'+ x +'/'+ contour_type +'-contours' for x in contour_files]
 
     for _path in contour_paths:
         path2contours, contours_avail = get_files_in_path(_path)
         all_path2contours.extend([path2contours + '/' + x for x in contours_avail])
 
     # fix: memoize for scalability
-    dicoms = [parsing.parse_dicom_file(DATA_PATH + '/dicoms/' + pairs[match_dicom_path(x)[0]] + '/' +
-                                       match_dicom_path(x)[1]) for x in all_path2contours]
-    contours = [{'mask': parsing.poly_to_mask(parsing.parse_contour_file(x), 265, 265),
+    dicoms = [parsing.parse_dicom_file(DATA_PATH + '/dicoms/' + pairs[match_dicom_path(x)[0]]
+                                       + '/' + match_dicom_path(x)[1]) for x in all_path2contours]
+    contours = [{'mask': parsing.poly_to_mask(parsing.parse_contour_file(x), 256, 256),
                  'file_path': x.split('/')[-3:]}
                 for x in all_path2contours]
-    return makeData(dicoms, contours)
+    return make_data(dicoms, contours)
 
+
+batch3=batch_wrapper(LINKFILE_PATH).next_batch(50)
 '''
-# visualize.plot_overlay(batch3[0][10], batch3[1][10], batch3[1][10]['file_path  # quick visual check
+visualize.plot_overlay(batch3[0][10], batch3[1][10], batch3[1][10]['file_path'])  # quick visual check
 '''
+# print(batch3[1][10])
